35d0183a4a19b84afd061c7301eaf800f8901b37
shahinkhalasi <shahinkhalasi@gmail.com>
2024-02-22 20:20:54 +0600

    added bus seats

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
-tailwind.config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {
-        colors: {
-          clifford: '#da373d',
-        }
-      }
-    }
+      extend: {},
+    },
+    plugins: [],
   }
